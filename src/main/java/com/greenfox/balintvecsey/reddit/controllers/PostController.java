25c4cb79b6052788b0c5a9b4f61fed934025d7a7
balintvecsey <balint.vecsey@gmail.com>
2017-09-05 12:40:40 +0200

    remove unnecessary lines

--- src/main/java/com/greenfox/balintvecsey/reddit/controllers/PostController.java
+++ src/main/java/com/greenfox/balintvecsey/reddit/controllers/PostController.java
@@ -5,13 +5,11 @@ import com.greenfox.balintvecsey.reddit.service.RedditService;
 import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
